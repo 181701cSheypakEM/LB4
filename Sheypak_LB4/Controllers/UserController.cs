5d1376d1a18a9912030b5ee4bd7e1b0c702883c9
181701_Evgenii_Sheypak <91116024+181701cSheypakEM@users.noreply.github.com>
2021-12-31 15:20:40 +0300

    Add files via upload

---
+++ Sheypak_LB4/Controllers/UserController.cs
@@ -0,0 +1,183 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using Sheypak_LB4.Models;
+
+namespace Sheypak_LB4.Models
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        private readonly IWebHostEnvironment _webHost;
+
+        public UserController(IWebHostEnvironment webHost)
+        {
+            _webHost = webHost;
+        }
+
+        [HttpGet("getUser")]
+        public IActionResult Get()
+        {
+            try
+            {
+                List<User> users = new();
+                FromFile(users);
+                return new JsonResult(users);
+            }
+            catch (Exception e)
+            {
+                return BadRequest(e.Message);
+            }
+        }
+
+        [HttpGet("getFile")]
+        public IActionResult GetFile()
+        {
+            try
+            {
+                string filePath = Path.Combine(_webHost.ContentRootPath, "test-image.png");
+                string fileType = "application/png";
+                string fileName = "image.png";
+                var path = PhysicalFile(filePath, fileType, fileName);
+                if (System.IO.File.Exists(path.FileName) == false){
+                    throw new Exception();
+                }
+                    
+                return path;
+            }
+            catch (Exception)
+            {
+                return BadRequest("Картинка не найдена");
+            }
+        }
+
+        [HttpPost("createUser")]
+        public IActionResult Post([FromBody] UserViewModel model)
+        {
+            try
+            {
+                List<User> users = new();
+                FromFile(users);
+                int id = 0;
+
+                if (users.Count == 0)
+                {
+                    users.Add(new User
+                    {
+                        Id = (id + 1).ToString(),
+                        UserName = model.UserName,
+                        Password = model.Password
+                    });
+                }
+                else
+                {
+                    var lastUser = users.Last();
+                    id = int.Parse(lastUser.Id);
+
+                    users.Add(new User
+                    {
+                        Id = (id + 1).ToString(),
+                        UserName = model.UserName,
+                        Password = model.Password
+                    });
+                }
+
+                ToFile(users);
+                return Ok("Пользователь  создан");
+            }
+            catch (Exception e)
+            {
+                return BadRequest(e.Message);
+            }
+        }
+
+        [HttpPut("updateUser")]
+        public IActionResult Put([FromQuery] string id, [FromBody] UserViewModel model)
+        {
+            try
+            {
+
+                List<User> users = new();
+                FromFile(users);
+
+                if (id == "0")
+                    return BadRequest("Напишите Id");
+                if (users.Count == 0)
+                    return BadRequest("Введены неккоректные данные или база данных пуста");
+
+                foreach (var i in users)
+                {
+                    if (i.Id == id)
+                    {
+                        i.UserName = model.UserName;
+                        i.Password = model.Password;
+                    }
+                }
+
+                ToFile(users);
+                return Ok("Пользователь успешно обновлен!");
+            }
+            catch (Exception e)
+            {
+                return BadRequest(e.Message);
+            }
+        }
+
+        [HttpDelete("deleteUserById")]
+        public IActionResult Delete([FromQuery] string id)
+        {
+            try
+            {
+                List<User> users = new();
+                FromFile(users);
+                var userUpdate = from user in users
+                                 where user.Id == id
+                                 select user;
+
+                users.Remove(userUpdate.First());
+                ToFile(users);
+                return new JsonResult(users);
+            }
+            catch (Exception e)
+            {
+                return BadRequest(e.Message);
+            }
+        }
+
+        private void ToFile(List<User> data)
+        {
+            using FileStream fstream = new("data.txt", FileMode.Create);
+            fstream.Seek(0, SeekOrigin.End);
+            StreamWriter sw = new(fstream);
+            foreach (User i in data)
+            {
+                sw.WriteLine(i.Id);
+                sw.WriteLine(i.UserName);
+                sw.WriteLine(i.Password);
+            }
+            sw.Close();
+        }
+
+        private void FromFile(List<User> data)
+        {
+            using FileStream fstream = new("data.txt", FileMode.OpenOrCreate);
+
+            StreamReader sw = new(fstream);
+            data.Clear();
+            while (!sw.EndOfStream)
+            {
+                User user = new();
+                user.Id = sw.ReadLine();
+                user.UserName = sw.ReadLine();
+                user.Password = sw.ReadLine();
+                data.Add(user);
+            }
+            sw.Close();
+
+        }
+    }
+}
---
+++ Sheypak_LB4/Models/UserViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Sheypak_LB4.Models
+{
+    public class UserViewModel
+    {
+        [Required(ErrorMessage = "Нужно ввести имя  пользователя")]
+        public string UserName { get; set; }
+
+        [Required(ErrorMessage = "Нужно ввести фамилию пользователя")]
+        public string Password { get; set; }
+    }
+}
